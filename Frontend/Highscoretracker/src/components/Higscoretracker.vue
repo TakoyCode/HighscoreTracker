3a425e567786602230d56626b55d3ae9fe2a0da5
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2025-02-05 14:51:10 +0100

    kim

--- Frontend/Highscoretracker/src/components/Higscoretracker.vue
+++ Frontend/Highscoretracker/src/components/Higscoretracker.vue
@@ -49,20 +49,21 @@ export default{
 </script>
 
 <template>
-
-        <div v-for="highScoreTracker in highScoreTrackers" :key="highScoreTracker.ID" style="display: grid;">
-                <div style="display: flex; margin-bottom: 10px;">
-                    <div>
-                        <span> <p>Streak: {{highScoreTracker.CurrentStreak}}</p></span>
-                        <span> <p>HighScore: {{highScoreTracker.HighScore}}</p></span>
-                        <span> <p>Total days: {{highScoreTracker.TotalDays}}</p></span>
-                    
+    <div class="mainmain">
+        <div class="main" v-for="highScoreTracker in highScoreTrackers" :key="highScoreTracker.ID">
+                <div style="display: flex;">
+                    <div class="content">
+                    <div class="para">
+                         <p>Streak: {{highScoreTracker.CurrentStreak}} </p>
+                         <p>HighScore: {{highScoreTracker.HighScore}} </p>
+                         <p>Total days: {{highScoreTracker.TotalDays}} </p>
+                    </div>
                     <br />
                     </div>
-                    <div>    
+                    <div class="content">    
                         <img :src="highScoreTracker.CurrentStreak > 0 ? '/img/happy.jpg' : '/img/sad.jpg'" alt="streak status">
                     </div>
-                    <div style="align-items: center; align-content: center;"> 
+                    <div class="content-button"> 
                     <button @click="increment(highScoreTracker.ID, highScoreTracker.CurrentStreak + 1)">Done</button>
                     <br />
                     <button @click="increment(highScoreTracker.ID, 0)">Not done</button>
@@ -70,6 +71,7 @@ export default{
                 </div>
                 <br/>
         </div>
+    </div>
        
 
 
@@ -79,20 +81,55 @@ export default{
 
 
 <style>
-
-img {
-    width: 80px;
-    height: 80px;
+.mainmain {
+    width: 100vw;
+}
+.main {
+    width: 60vw;
+    height: 25vh;
+    background: rgba(33, 155, 166, 0.3);
+    border-radius: 10px;
+    margin-bottom: 5px;
 }
 
-p {
-    width: 100px;
-    height: 20px;
+.content {
+    display: flex;
+    width: 33%;
+    height: 25vh;
+    margin: 0;
+    text-align: center;
+    justify-content: center;
+    font-size: larger;
+    font-family: 'Courier New', Courier, monospace;
+    font-weight: bold;
+}
+
+.content:hover {
+    background: rgba(33, 155, 90, 0.8);
+}
+.para {
+    display: grid;
+    text-align: right;
+}
+img {
+   
+    width: 20vh;
+    height: 20vh;
+}
+
+
+content-button {
+    width: 100%;
+    height: 100%;
 }
 
 button {
-    width: 100px;
+    width: 200px;
+    height: 25px;;
+}
 
+button:hover {
+    background: rgba(33, 55, 166, 0.9);
 }
 
 </style>
