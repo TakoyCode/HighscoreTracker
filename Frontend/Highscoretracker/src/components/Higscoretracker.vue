e846fce1f4aed3f1a1e8b75c753f7190b5723ed2
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2025-02-05 14:17:52 +0100

    kim

--- Frontend/Highscoretracker/src/components/Higscoretracker.vue
+++ Frontend/Highscoretracker/src/components/Higscoretracker.vue
@@ -5,42 +5,94 @@ import axios from 'axios';
 export default{
     data(){
     return {
-        highScoreTrackers:[]
+        highScoreTrackers:[{
+            CurrentStreak: 0,
+            HighScore: 0,
+            TotalDays: 0,
+        },
+        {
+            CurrentStreak: 0,
+            HighScore: 0,
+            TotalDays: 0,
+        }]
     }
     },
     methods: {
         async fetchCount(){
-            const response = await axios.get("http://localhost:3000/counter");
+            const response = await axios.get("https://localhost:7094/HighScores");
             this.highScoreTrackers = response;
         },
-        async increment(id) {
-            const en = highScoreTrackers.find(h => h.Id == id)
-            en.CurrentStreak++
-            const response = await axios.post("http://localhost:3000/HighScore", en)
+        async increment(id, num) {
+            const highScoreTracker = highScoreTrackers.find(h => h.Id == id)
+            highScoreTracker.CurrentStreak = num;
+            const response = await axios.post("https://localhost:7094/HighScores", highScoreTracker)
             this.fetchCount()
         },
-        async 
+        async AddHighScoreTrackers(){
+            const highScoreTracker = new {
+                CurrentStreak: 0,
+                HighScore: 0,
+                TotalDays: 0,
+            }
+            const response = await axios.post("https://localhost:7094/HighScores", highScoreTracker)
+            this.fetchCount()
+        },
+
+        async DeleteHighScoreTrackers(id){
+            const highScoreTracker = highScoreTrackers.find(h => h.Id == id)
+            const response = await axios.delete("https://localhost:7094/HighScores", highScoreTracker)
+            this.fetchCount()
+        }
     
     }
 }
-[
-    {
-        "CurrentStreak": 1,
-        "HighScore": 0,
-        "TotalDays": 0,
-        "ID": 1
-    }
-]
 </script>
 
 <template>
-<Div>{{highScoreTrackers.CurrentStreak}} - {{count > 0 ? "/assets/img/" : ":-(" }}</Div>
-<button @click="increment">Done</button>
-<button @click="reset">Not done</button>
+
+        <div v-for="highScoreTracker in highScoreTrackers" :key="highScoreTracker.ID" style="display: grid;">
+                <div style="display: flex; margin-bottom: 10px;">
+                    <div>
+                        <span> <p>Streak: {{highScoreTracker.CurrentStreak}}</p></span>
+                        <span> <p>HighScore: {{highScoreTracker.HighScore}}</p></span>
+                        <span> <p>Total days: {{highScoreTracker.TotalDays}}</p></span>
+                    
+                    <br />
+                    </div>
+                    <div>    
+                        <img :src="highScoreTracker.CurrentStreak > 0 ? '/img/happy.jpg' : '/img/sad.jpg'" alt="streak status">
+                    </div>
+                    <div style="align-items: center; align-content: center;"> 
+                    <button @click="increment(highScoreTracker.ID, highScoreTracker.CurrentStreak + 1)">Done</button>
+                    <br />
+                    <button @click="increment(highScoreTracker.ID, 0)">Not done</button>
+                    </div>
+                </div>
+                <br/>
+        </div>
+       
+
+
+
 </template>
 
 
 
 <style>
+
+img {
+    width: 80px;
+    height: 80px;
+}
+
+p {
+    width: 100px;
+    height: 20px;
+}
+
+button {
+    width: 100px;
+
+}
 
 </style>
